02f913bf765fe214d1844d7afd71025e53319dba
l <eckeriaue@gmail.com>
2024-10-19 23:19:40 +0300

    format (chain call)

--- index.mjs
+++ index.mjs
@@ -1,15 +1,28 @@
 // Import the framework and instantiate it
 import Fastify from 'fastify'
+import { env } from 'node:process'
+
+const port = parseInt(env.PORT || '3000')
+const host = env.HOST || '127.0.0.1'
+
 const app = Fastify({
   logger: true
 })
 
-app.register(import('@fastify/multipart'))
-app.register(import('./converterPlugin.mjs'))
-
-try {
-  await app.listen({ port: 3000 })
-} catch (err) {
+app
+.register(import('@fastify/multipart'))
+.register(import('./converterPlugin.mjs'))
+.listen({ port, host })
+.then(() => {
+  console.info(
+    `
+      image converted was started
+      host: ${host}
+      port: ${port}
+    `
+  )
+})
+.catch(err => {
   app.log.error(err)
   process.exit(1)
-}
+})
