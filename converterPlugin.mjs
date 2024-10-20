b9c492c8d186368e23d9460a3a06896f184d53e6
l <eckeriaue@gmail.com>
2024-10-20 14:08:58 +0300

    add fastify static

--- converterPlugin.mjs
+++ converterPlugin.mjs
@@ -22,57 +22,18 @@ async function extractFilesFromRequest(request) {
   return files
 }
 
+/**
+ * @param {import('fastify').FastifyInstance} app
+*/
 export default function any2webpConverterPlugin(app, { endpoint = '/convert' } = {}, done) {
   app.post(endpoint, async function(request, reply) {
     reply.header('Content-Type', 'multipart/form-data')
     const files = await extractFilesFromRequest(request)
     return sharp(files.at(0).content).webp().toBuffer()
   })
-  
-  app.get('/', async function  (request, reply) {
-    reply.header('Content-Type', 'text/html')
-    return /* html */`
-    <div>
-      <form name="sendForm" style="display: flex">
-        <input name="targetFileInput" type="file">
-        <button type="submit"> convert </button>
-        <img id="previewInput" style="width: 400px" />
-        <img id="previewOutput" style="width: 400px" />
-      </form>
-      <script>
-  
-        document.forms.sendForm.targetFileInput.addEventListener('input', function(event) {
-          const [file] = event.target.files
-          if (previewInput.src) {
-            URL.revokeObjectURL(previewInput.src)
-          }
-          previewInput.src = URL.createObjectURL(file)
-        })
-  
-        document.forms.sendForm.addEventListener('submit', async function(event) {
-          event.preventDefault()
-          const [file] = event.target.targetFileInput.files
-          const fd = new FormData(event.target)
-          fd.append('targetFile', file)
-          const requestBlob = await fetch('/convert', {
-            method: 'POST',
-            body: fd,
-          })
-          .then(file => file.blob())
-          .catch(e => {
-            console.error('Файл не был загружен')
-          })
-  
-          previewOutput.src = URL.createObjectURL(
-            new File(
-              [requestBlob],
-              'output.webp',
-              { type: 'image/webp' }
-            )
-          )
-        })
-      </script>
-    </div>`
+
+  .get('/', function (request, reply) {
+    reply.sendFile('client.html')
   })
   done()
 }
--- index.mjs
+++ index.mjs
@@ -1,15 +1,20 @@
 import Fastify from 'fastify'
+import { join, resolve } from 'node:path'
 import { env } from 'node:process'
+import { fileURLToPath } from 'node:url'
 
 const port = parseInt(env.PORT || '3000')
 const host = env.HOST || '127.0.0.1'
 
 const app = Fastify({
-  logger: true
+  logger: true,
 })
 
 app
 .register(import('@fastify/multipart'))
+.register(import('@fastify/static'), {
+  root: fileURLToPath(new URL('./public', import.meta.url)),
+})
 .register(import('./converterPlugin.mjs'))
 .listen({ port, host })
 .then(() => {
